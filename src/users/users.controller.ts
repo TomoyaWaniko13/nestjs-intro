e6cd9e2e37c2d97fe623944555ac66f1e6d3a698
tomoya <alligatorfree12@gmail.com>
2024-10-04 02:19:46 +0900

    23. Validating Query Params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Get, Headers, Ip, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, Headers, Ip, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 
 // 15. Creating Controllers
 // 17. Params, Query and Body
 // 18. Additional Request Components
 // 21. Validation and Transformation Needs
 // 22. Validating Params with Built-in Pipes
+// 23. Validating Query Params
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
-  public getUsers(@Param('id', ParseIntPipe) id: number | undefined, @Query('limit') limit: any) {
-    console.log(typeof id);
-    console.log(id);
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit);
+    console.log(page);
     return 'You sent a get request to the users endpoint';
   }
 
