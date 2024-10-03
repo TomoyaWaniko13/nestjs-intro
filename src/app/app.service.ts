f4a5033f4056339e438cc0f7109432c7a5ca0825
tomoya <alligatorfree12@gmail.com>
2024-10-03 12:56:48 +0900

    11. Understanding the app Module

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+// 11. Understanding the app Module
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello From Nest.js!';
   }
 }
