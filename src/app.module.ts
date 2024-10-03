91ead880aa7bc537f5acba01b6f2f5762683759f
tomoya <alligatorfree12@gmail.com>
2024-10-03 13:13:11 +0900

    12. Creating a New users Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+// 12. Creating a New users Module
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
