0680d830acde033edc3e7ee52ed0c431d11f2b62
tomoya <alligatorfree12@gmail.com>
2024-10-03 18:42:34 +0900

    18. Additional Request Components

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,23 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Ip, Param, Post, Query } from '@nestjs/common';
 
 // 15. Creating Controllers
 // 17. Params, Query and Body
+// 18. Additional Request Components
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'You sent a get request to the users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
+  public createUsers(@Body() request: any, @Headers() headers: any, @Ip() ip: any) {
     console.log(request);
+    console.log(headers);
+    console.log(ip);
     return 'You sent a post request to users endpoint';
   }
 }
