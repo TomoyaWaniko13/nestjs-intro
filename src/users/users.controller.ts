b26907fc86e1382e1d34ef1d8da772ecf6ab011f
tomoya <alligatorfree12@gmail.com>
2024-10-04 02:02:19 +0900

    22. Validating Params with Built-in Pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Get, Headers, Ip, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Ip, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 
 // 15. Creating Controllers
 // 17. Params, Query and Body
 // 18. Additional Request Components
 // 21. Validation and Transformation Needs
+// 22. Validating Params with Built-in Pipes
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+  @Get('/:id?')
+  public getUsers(@Param('id', ParseIntPipe) id: number | undefined, @Query('limit') limit: any) {
     console.log(typeof id);
-    console.log(typeof limit);
+    console.log(id);
     return 'You sent a get request to the users endpoint';
   }
 
