3eaf15c65945b5de9a06ced0a7d2f353fed32b5e
tomoya <alligatorfree12@gmail.com>
2024-10-23 15:34:59 +0900

    40. Return Posts From Posts Service

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 // 37. Solution: Create a Posts Module
+// 39. Create a GET Posts Route With DTO
+// 40. Return Posts From Posts Service
 @Injectable()
 export class PostsService {
   public findAll(userId: string) {
-    console.log(userId);
+    return [
+      { title: 'Test Title', content: 'Test Content' },
+      { title: 'Test Title 2', content: 'Test Content 2' },
+    ];
   }
 }
