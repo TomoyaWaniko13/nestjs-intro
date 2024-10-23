40b9c27d6ca7866595956015fa091e9fca5fd0c6
tomoya <alligatorfree12@gmail.com>
2024-10-23 15:29:06 +0900

    39. Create a GET Posts Route With DTO

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './providers/posts.service';
 
 // 37. Solution: Create a Posts Module
+// 39. Create a GET Posts Route With DTO
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  // Injecting Posts Service
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get('/:userId?')
+  public getPosts(@Param('userId') userId: string) {
+    return this.postsService.findAll(userId);
+  }
+}
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 // 37. Solution: Create a Posts Module
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  public findAll(userId: string) {
+    console.log(userId);
+  }
+}
