3cf228d499bea33a096c4b7343c8f55ecc1d35fd
tomoya <alligatorfree12@gmail.com>
2024-10-24 12:05:50 +0900

    52. Adding Validations To CreatePostDto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,18 +1,53 @@
 import { postType } from '../enum/postType.enum';
 import { postStatus } from '../enum/postStatus.enum';
+import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from 'class-validator';
 
 // 51. Solution: POST Endpoint and DTO For Posts Controller
+// 52. Adding Validations To CreatePostDto
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
+
+  @IsEnum(postType)
+  @IsNotEmpty()
   postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
   slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
   status: postStatus;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
   publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
   tags?: string[];
+
   // https://basarat.gitbook.io/typescript/type-system/index-signatures
   metaOptions: [{ key: 'sidebarEnabled'; value: true }];
 }
